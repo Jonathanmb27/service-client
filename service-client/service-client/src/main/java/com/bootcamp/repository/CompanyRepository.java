f90c1cdd172bf1cc4d460bb4ad4fe22cabdabc13
Jonathanmb27 <73472449+Jonathanmb27@users.noreply.github.com>
2022-06-17 10:47:59 -0500

    add more data

--- service-client/service-client/src/main/java/com/bootcamp/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.repository;
-
-import com.bootcamp.dao.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company,String> {
-}
--- service-client/service-client/src/main/java/com/bootcamp/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.repository;
-
-import com.bootcamp.dao.Person;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PersonRepository extends MongoRepository<Person,String> {
-}
