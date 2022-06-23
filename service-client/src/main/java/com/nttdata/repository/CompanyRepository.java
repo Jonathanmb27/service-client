81b544a23ac436528767ac167b652cfb752e8098
Jonathanmb27 <73472449+Jonathanmb27@users.noreply.github.com>
2022-06-22 23:57:36 -0500

    update

--- service-client/src/main/java/com/nttdata/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.repository;
-
-import com.nttdata.domain.dao.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company, String> {
-}
